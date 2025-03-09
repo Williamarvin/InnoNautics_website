e209fe2bb71e50ad70d68853d1cacb22b15dd25c
Williamarvin <williamarvin111@gmail.com>
2025-03-09 17:05:39 +0800

    contact us backend wip

---
+++ php/contact_form.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["comment"]);
+
+    // Your email where you want to receive messages
+    $to = "williamarvin222@gmail.com";  // Change this to your actual email
+    $subject = "New Contact Form Message from $name";
+
+    // Email headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email content
+    $email_body = "You have received a new message from your website contact form.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Send the email
+    if (mail($to, $subject, $email_body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
